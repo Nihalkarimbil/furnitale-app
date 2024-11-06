5fe0f8c8b4377c5aaa30cfc2716d72efcd5ca1c5
Nihalkarimbil <muhammednihalk4@gmail.com>
2024-11-06 13:24:15 +0530

    changes

--- src/First/Image.jsx
+++ src/First/Image.jsx
@@ -10,7 +10,7 @@ function Image() {
         <div className="text-white sm:text-lg md:text-xl lg:text-2xl xl:text-5xl ml-10 " id='main'>
           Design Your Story...<br/>Frame by Frame          
           <div className="absolute inset-x-0">
-          <button className="text-sm h-7 sm:h-9 md:h-11 lg:h-13 xl:h-15  border rounded p-2 ml-11 hover:bg-red-400" >Explore</button>
+          <button className="text-sm h-7 sm:h-9 md:h-11 lg:h-13 xl:h-10  border rounded p-2 ml-11 hover:bg-red-400" >Explore</button>
           </div>
         </div>
       </div>
--- src/First/Newcoll.jsx
+++ src/First/Newcoll.jsx
@@ -31,8 +31,7 @@ function Newcoll() {
   return (
     <div className="bg-gray-100 py-8">
       <h1 className="flex justify-center text-4xl text-gray-800" id='category'>New Collections</h1>
-      <p className="flex justify-center font-sans text-base text-black mb-8" id='sub'>Explore Limitless Possibilities with Our New Collection</p>
-      
+
       <div className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {coll.map((product) => (
