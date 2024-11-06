46d632f7c731252c9f2dde9682ce8957916f8466
Nihalkarimbil <muhammednihalk4@gmail.com>
2024-11-06 13:27:08 +0530

    ...

--- src/First/Newcoll.jsx
+++ src/First/Newcoll.jsx
@@ -30,7 +30,7 @@ function Newcoll() {
 
   return (
     <div className="bg-gray-100 py-8">
-      <h1 className="flex justify-center text-4xl text-gray-800" id='category'>New Collections</h1>
+      <h1 className="flex justify-center text-4xl text-gray-800 mb-10" id='category '>New Collections</h1>
 
       <div className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
