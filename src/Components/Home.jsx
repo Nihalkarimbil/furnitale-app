e1f91b1b6413efdbe6cb19aefdf07d52314f3e34
Nihalkarimbil <muhammednihalk4@gmail.com>
2024-11-06 15:07:10 +0530

    up

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
-import Image from '../First/Image'
-import Trending from '../First/Trending'
-import Newcoll from '../First/Newcoll'
-import { useContext } from 'react'
-import { Procontext } from '../context/Productcontext'
-import LoadingSpinner from '../Spinner'
-
+import React, { useContext } from 'react';
+import Image from '../First/Image';
+import Trending from '../First/Trending';
+import Newcoll from '../First/Newcoll';
+import { Procontext } from '../context/Productcontext';
+import LoadingSpinner from '../Spinner';
 
 function Home() {
   const { loading } = useContext(Procontext);
@@ -14,15 +12,17 @@ function Home() {
     <div>
       <Image />
       <div>
-        { loading ? (<LoadingSpinner />) : (
-        <Newcoll />,
-        <Trending />
+        {loading ? (
+          <LoadingSpinner />
+        ) : (
+          <>
+            <Newcoll />
+            <Trending />
+          </>
         )}
-
       </div>
-
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
