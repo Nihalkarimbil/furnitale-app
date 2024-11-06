74d106056bde8d28c3f94e49bb3f2eee28de8079
Nihalkarimbil <muhammednihalk4@gmail.com>
2024-11-06 15:20:12 +0530

    spinnerup

--- src/Spinner.jsx
+++ src/Spinner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LoadingSpinner = () => {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center w-full h-screen bg-white">
       <div className="w-8 h-8 border-4 border-t-4 border-blue-500 rounded-full animate-spin"></div>
     </div>
   );
