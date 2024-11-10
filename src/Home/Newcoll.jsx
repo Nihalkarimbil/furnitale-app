238366e4d869c29ca15fef5e00fc82c4d244be99
Nihalkarimbil <muhammednihalk4@gmail.com>
2024-11-10 11:20:13 +0530

    linking issue

--- src/Home/Newcoll.jsx
+++ src/Home/Newcoll.jsx
@@ -37,8 +37,8 @@ function Newcoll() {
       <div className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {coll.map((product) => (
-            <div key={product.id} className="group relative" data-aos="fade-up">
-              <Link to={`/product/${product.id}`} className="block aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 shadow-lg xl:aspect-w-7 xl:aspect-h-8">
+            <div key={product._id} className="group relative" data-aos="fade-up">
+              <Link to={product._id}className="block aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 shadow-lg xl:aspect-w-7 xl:aspect-h-8">
                 <img
                   src={product.image}
                   alt={product.description}
